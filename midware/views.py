7bee95f6d4fd99d1f3745e54a1b3f139bb039df6
forewing <jujianai@hotmail.com>
2018-09-23 12:25:56 +0800

    add icon

--- midware/views.py
+++ midware/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect
 def index(request):
     return HttpResponse("index")
 
 def favicon(request):
-    return HttpResponse("")
+    image_data = open('5ba7145ad390f.32px.ico', 'rb').read()
+    return HttpResponse(image_data, content_type="image/png")
